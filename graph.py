6248bb0448ef7b82b19cadc2b7aae835bdbc10c6
JoelO <jornstein@hmc.edu>
2012-02-10 17:26:28 -0800

    Update graph.py

--- graph.py
+++ graph.py
@@ -5,21 +5,23 @@ class graph:
         self.notConspRec = -.8
         self.conspSend = 1
         self.conspRec = .5
-        self.MessageWeight = {1:0,\
-                              2:0,\
-                              3:0,\
-                              4:0,\
-                              5:0,\
-                              6:0,\
-                              7:1,\
-                              8:0,\
-                              9:0,\
-                              10:0,\
-                              11:1,\
-                              12:0,\
-                              13:1,\
-                              14:0,\
-                              15:0}
+        self.topicCount = [0]*16
+        self.topicWeight = [0,  #Not in use\
+                            0,  #1\
+                            0,  #2\
+                            0,  #3\
+                            0,  #4\
+                            0,  #5\
+                            0,  #6\
+                            1,  #7\
+                            0,  #8\
+                            0,  #9\
+                            0,  #10\
+                            1,  #11\
+                            0,  #12\
+                            1,  #13\
+                            0,  #14\
+                            0]  #15
         f = open(rFile, 'r')
         for line in f.readlines():
             Nums = line.split()
@@ -29,19 +31,39 @@ class graph:
                     self.D[(Node1,Node2)] = []
             for i in Nums[2:]:
                 self.D[(Node1,Node2)] += [int(i)]
+                self.topicCount[int(i)]+=1
         f.close()
 
     def weightConspirator(self, node, known = 1):
-        for i in range(1,83):
-            for j in self.D[(node,i)]:
-                self.MessageWeight[j]+= known * self.conspSend
-            for j in self.D[(i,node)]:
-                self.MessageWeight[j]+= known * self.conspRecieve
+        for i in range(83):
+            if self.D.has_key((node,i)):
+                for j in self.D[(node,i)]:
+                    self.topicWeight[j]+= known * self.conspSend
+            if self.D.has_key((i,node)):
+                for j in self.D[(i,node)]:
+                    self.topicWeight[j]+= known * self.conspRec
+
+    def numSent(self, node, topic):
+        count = 0
+        for i in range(83):
+            if self.D.has_key((node,i)):
+                for j in self.D[(node,i)]:
+                    if j == topic: count+=1
+        return count
+        
+    def numRec(self, node, topic):
+        count = 0
+        for i in range(83):
+            if self.D.has_key((i, node)):
+                for j in self.D[(i, node)]:
+                    if j == topic: count+=1
+        return count
 
     def weightNotConspirator(self, node, known = 1):
-        for i in range(1,83):
-            for j in self.D[(node,i)]:
-                self.MessageWeight[j]+= known * self.notConspSend
-            for j in self.D[(i,node)]:
-                self.MessageWeight[j]+= known * self.notConspRecieve
-        
+        for i in range(83):
+            if self.D.has_key((node,i)):
+                for j in self.D[(node,i)]:
+                    self.topicWeight[j]+= known * self.notConspSend
+            if self.D.has_key((i,node)):
+                for j in self.D[(i,node)]:
+                    self.topicWeight[j]+= known * self.notConspRec
