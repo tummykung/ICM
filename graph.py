8c214f622636aa9f6a2bb13e0ff89205e7186856
JoelO <jornstein@hmc.edu>
2012-02-10 16:48:51 -0800

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,7 +1,25 @@
 class graph:
     def __init__(self,rFile):
         self.D = {}
-        self.MessageWeight = {}
+        self.notConspSend = -.5
+        self.notConspRec = -.8
+        self.conspSend = 1
+        self.conspRec = .5
+        self.MessageWeight = {1:0,\
+                              2:0,\
+                              3:0,\
+                              4:0,\
+                              5:0,\
+                              6:0,\
+                              7:1,\
+                              8:0,\
+                              9:0,\
+                              10:0,\
+                              11:1,\
+                              12:0,\
+                              13:1,\
+                              14:0,\
+                              15:0}
         f = open(rFile, 'r')
         for line in f.readlines():
             Nums = line.split()
@@ -12,3 +30,18 @@ class graph:
             for i in Nums[2:]:
                 self.D[(Node1,Node2)] += [int(i)]
         f.close()
+
+    def weightConspirator(self, node, known = 1):
+        for i in range(1,83):
+            for j in self.D[(node,i)]:
+                self.MessageWeight[j]+= known * self.conspSend
+            for j in self.D[(i,node)]:
+                self.MessageWeight[j]+= known * self.conspRecieve
+
+    def weightNotConspirator(self, node, known = 1):
+        for i in range(1,83):
+            for j in self.D[(node,i)]:
+                self.MessageWeight[j]+= known * self.notConspSend
+            for j in self.D[(i,node)]:
+                self.MessageWeight[j]+= known * self.notConspRecieve
+        
